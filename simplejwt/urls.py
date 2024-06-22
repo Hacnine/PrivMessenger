57f68c91e7129e5ce4cd53418559e3db484748e0
hacnine <hacninetusar@gmail.com>
2024-06-22 15:13:16 +0600

    GenericViewApi

--- simplejwt/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
-
-urlpatterns = [
-    path('gettotken/', TokenObtainPairView.as_view(), name='gettotken'),
-    path('refreshtoken', TokenRefreshView.as_view(), name='refreshtoken'),
-    path('tokenverify/', TokenVerifyView.as_view(), name='tokenverify'),
-]
