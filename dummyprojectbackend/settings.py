5142232c4f576bff121f368f05babc4ce4e477f9
hacnine <hacninetusar@gmail.com>
2024-08-12 23:58:04 +0600

    Minor Changes

--- dummyprojectbackend/settings.py
+++ dummyprojectbackend/settings.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 SECRET_KEY = 'django-insecure-adnazvpoyik3q+(k12$%!y!ancwtm&g!zc3u5m%_e3v!)0xr3l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
